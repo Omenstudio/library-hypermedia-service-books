bb0672f7e7b94801686affdb7b73d1e2096aeb42
Vasily <vasily.rutin@gmail.com>
2017-11-26 16:47:33 +0400

    Spring added

---
+++ src/main/java/com/github/omenstudio/weblibrary/entity/Book.java
@@ -0,0 +1,34 @@
+package com.github.omenstudio.weblibrary.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getId() {
+
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
